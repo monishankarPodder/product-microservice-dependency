2e392a01ee45fff84705d9248a5565ef255bacc6
monishankarPodder <monishankar.podder@gmail.com>
2025-07-20 20:11:03 +0530

    Update ProductService.java

--- src/main/java/com/example/product/service/ProductService.java
+++ src/main/java/com/example/product/service/ProductService.java
@@ -35,7 +35,7 @@ public class ProductService {
 
     // 🆕 Additional method called by delete()
     public void logDeleteAction(Long id) {
-        int i= 299;
+        int i= 2449;
         System.out.println("LOG: deleting product with id = " + id);
     }
 }
